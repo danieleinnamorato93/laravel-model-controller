b4a598eb962bf7b3a7c3b1c7f4103a181bb243b9
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-10-24 18:28:42 +0200

    esercizio terminato

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -4,7 +4,36 @@
 
 @section("main-content")
 <h1>
-        Movies
+        Movies:
 </h1>
-{{--@dd($movies);--}}
+{{--@dd($movies);
+  #attributes: array:6 [▼
+        "id" => 3
+        "title" => "Psycho"
+        "original_title" => "Psycho"
+        "nationality" => "american"
+        "date" => "1960-06-16"
+        "vote" => 9.4
+      ]--}}
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-12">
+          @forelse($movies as $movie)
+            <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title">{{$movie->title}}</h4>
+                  <h6 class="card-text">{{$movie->nationality}}</h6>
+                  <p class="card-text">{{$movie->date}}</p>
+                  <p class="card-text">{{$movie->vote}}</p>
+                
+                  
+                </div>
+                @empty
+                <p>no movies available!</p>
+                @endforelse
+              </div>
+
+        </div>
+    </div>
+</div>
 @endsection
